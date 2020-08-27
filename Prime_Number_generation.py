fad78bdd815cffeca172cd83c99e88a0974d2633
Vidhya-Arjun <53186437+Vidhya-Arjun@users.noreply.github.com>
2020-08-27 10:54:46 +0200

    Create Prime_Number_generation.py
    Algorithm:
    Prime Number generation between two ranges. 
    - Get the input from user that is minimum and maximum value.
    - If Minimum value is less than Maximum value Start the prime number generation else print message stating Minimum value is greater than than Maximum value.
    -

---
+++ Prime_Number_generation.py
@@ -0,0 +1,18 @@
+Intializer = 0 # Intializer is a counter helps to denote the starting point
+while(Intializer==0):  # Condition to start the program
+   
+    Minimum = int(input("Enter the Minimum value for prime number generation: "))
+    Maximum = int(input("Enter the Maximum value for prime number generation: "))
+
+    if (Maximum > Minimum):  # Checking Minimum is less than Maximum value
+        Intializer += 1
+        for Number in range(Minimum,Maximum):  # Intializing for loop between the ranges
+            count = 0
+            for i in range(2,Number//2 + 1): #  
+                if(Number%i ==0):
+                    count += 1
+                    break
+            if(count==0 and Number!= 1):
+                print(Number)
+    else:   
+        print("Minimum value is greater than value, Please enter Minimum value less than Maximum value")
